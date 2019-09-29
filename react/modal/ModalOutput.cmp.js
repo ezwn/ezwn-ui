caedb3d369f0096a9b4198a9a645a2cdb543dd84
Nicolas ENZWEILER <nicolas@ileotech.com>
2019-09-29 17:48:27 +0200

    New component : Modal

---
+++ react/modal/ModalOutput.cmp.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ModalContext from "./Modal.context";
+
+import "./ModalOutput.cmp.css";
+
+const ModalOutputCmp = props => {
+  const { content } = props;
+
+  if (!content) return <div />;
+
+  return ReactDOM.createPortal(
+    <ModalContext.Consumer>
+      {showModal => (
+        <div className={`modal-background`} onClick={() => showModal(null)}>
+          <div className={`modal`} onClick={e => e.stopPropagation()}>
+            {content}
+          </div>
+        </div>
+      )}
+    </ModalContext.Consumer>,
+    document.getElementById("modalOutput")
+  );
+};
+
+export const ModalProviderCmp = ({ children }) => {
+  const [content, showModal] = useState(null);
+
+  return (
+    <ModalContext.Provider value={showModal}>
+      <ModalOutputCmp content={content} />
+      {children}
+    </ModalContext.Provider>
+  );
+};
